5ef458ed2e04c495ecfd02a477b9cf86d77b67f0
Ivan <fenomen_ny@mail.ru>
2022-03-27 14:14:07 +0300

    our-best complete

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,21 +1,32 @@
 import './cards.css';
+import img from './../../img/bestItem.jpg';
+import img2 from './../../img/bestItem2.jpg';
+import img3 from './../../img/bestItem3.jpg';
 
 const Cards = () => {
   return (
-    // <div class="row justify-content-md-center">
-    //   <div class="col-md-3 offset-md-1">Один из трех столбцов столбцов </div>
-    //   <div class="col-md-3">Один из трех столбцов столбцов</div>
-    //   <div class="col-md-3 offset-md-1">Один из трех столбцов столбцов</div>
-    // </div>
-    // <div class="row justify-content-md-center">
-    //   <div class="col-md-3 offset-md-1">Один из трех столбцов столбцов</div>
-    //   <div class="col-md-3 offset-md-1">Один из трех столбцов столбцов</div>
-    //   <div class="col-md-3 offset-md-1">Один из трех столбцов столбцов</div>
-    // </div>
-    <div class="row justify-content-md-evenly">
-      <div class="col-md-3">Один из трех столбцов столбцов</div>
-      <div class="col-md-3">Один из трех столбцов столбцов</div>
-      <div class="col-md-3">Один из трех столбцов столбцов</div>
+    <div className="cards row mt-4 justify-content-md-evenly">
+      <div className="col-md-3 offset-md-1">
+        <div className="wrap">
+          <img src={img} alt="card" className="mx-auto d-block" />
+          <h6 className="text-center mt-3">Solimo Coffee Beans 2 kg</h6>
+          <div className="price text-end">10.73$</div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="wrap">
+          <img src={img2} alt="card" className="mx-auto d-block" />
+          <h6 className="text-center mt-3 h6-2">Presto Coffee Beans 1 kg</h6>
+          <div className="price text-end">15.99$</div>
+        </div>
+      </div>
+      <div className="cards3 col-md-3 offset-md-1">
+        <div className="wrap">
+          <img src={img3} alt="card" className="mx-auto d-block img3" />
+          <h6 className="text-center mt-3">AROMISTICO Coffee 1 kg</h6>
+          <h5 className="price text-end">6.99$</h5>
+        </div>
+      </div>
     </div>
   );
 };
